bd547884c3db027d672e0f3fd1b6183b27b796af
Marty Forde <marty@insightsoftmax.com>
2025-04-11 15:47:48 -0500

    forde-commit-1744404468

--- run2.sh
+++ run2.sh
@@ -228,13 +228,13 @@ autoinstall:
     # Ensure user exists in the installed system
     - curtin in-target --target=/target -- bash -c "getent passwd ${USERN} > /dev/null || useradd -m -s /bin/bash ${USERN}"
     - curtin in-target --target=/target -- bash -c "echo \"${USERN}:${PASSWORD}\" | chpasswd"
-    - curtin in-target --target=/target -- bash -c "echo \"${USERN} ALL=(ALL) NOPASSWD:ALL\" > /target/etc/sudoers.d/${USERN}"
-    - curtin in-target --target=/target -- bash -c "chmod 440 /target/etc/sudoers.d/${USERN}"
+    - curtin in-target --target=/target -- bash -c "echo \"${USERN} ALL=(ALL) NOPASSWD:ALL\" > /etc/sudoers.d/${USERN}"
+    - curtin in-target --target=/target -- bash -c "chmod 440 /etc/sudoers.d/${USERN}"
 
     # Configure SSH with X11 forwarding
-    - curtin in-target --target=/target -- bash -c "sed -i 's/#X11Forwarding no/X11Forwarding yes/' /target/etc/ssh/sshd_config"
-    - curtin in-target --target=/target -- bash -c "sed -i 's/#X11DisplayOffset 10/X11DisplayOffset 10/' /target/etc/ssh/sshd_config"
-    - curtin in-target --target=/target -- bash -c "sed -i 's/#X11UseLocalhost yes/X11UseLocalhost yes/' /target/etc/ssh/sshd_config"
+    - curtin in-target --target=/target -- bash -c "sed -i 's/#X11Forwarding no/X11Forwarding yes/' /etc/ssh/sshd_config"
+    - curtin in-target --target=/target -- bash -c "sed -i 's/#X11DisplayOffset 10/X11DisplayOffset 10/' /etc/ssh/sshd_config"
+    - curtin in-target --target=/target -- bash -c "sed -i 's/#X11UseLocalhost yes/X11UseLocalhost yes/' /etc/ssh/sshd_config"
     - curtin in-target --target=/target -- systemctl restart ssh
 
     # Eunsure bzip2 is present
